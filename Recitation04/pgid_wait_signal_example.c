d45254771d0559f23e68c96166c70d6c623b72c6
Seungmin Han <k.seungminhan@gmail.com>
2023-10-03 11:32:19 -0400

    tiny update

--- Recitation04/pgid_wait_signal_example.c
+++ Recitation04/pgid_wait_signal_example.c
@@ -7,21 +7,21 @@
 int main() {
   int status;
   pid_t pid1 = fork();
-  setpgid(pid1, pid1);
+  setpgid(pid1, pid1); //comment
   if (pid1 == 0) {
     char* args[] = {"sleep", "1000", NULL};
     if (execvp(args[0], args) < 0) perror("exec");
   }
 
   pid_t pid2 = fork();
-  setpgid(pid2, pid1);
+  setpgid(pid2, pid1); //comment
   if (pid2 == 0) {
     char* args[] = {"echo", "hi", NULL};
     if (execvp(args[0], args) < 0) perror("exec");
   }
 
   pid_t pid3 = fork();
-  setpgid(pid3, pid1);
+  setpgid(pid3, pid1); //comment
   if (pid3 == 0) {
     sleep(5);
     fprintf(stderr, "sending stop!\n");
